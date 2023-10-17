f0f35daeb77512b0b5155971e9dd46ff9b654e81
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-10-17 23:56:38 +0530

    update product page

--- front-end/src/Components/UpdateProduct.js
+++ front-end/src/Components/UpdateProduct.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router'
 
 const UpdateProduct = ()=>{
     const [ name, setName ] = React.useState('')
     const [ price, setPrice ] = React.useState('')
     const [ category, setCategory ] = React.useState('')
     const [ company, setCompany ] = React.useState('')
+    const params = useParams()
+
+    useEffect(()=>{
+        getProductDetails()
+    },[])
+
+    const getProductDetails = async ()=>{
+        console.log("params", params)
+        let result = await fetch(`http://localhost:5000/product/${params.id}`)  // this will give output in string
+        result = await result.json()
+        setName(result.name)
+        setPrice(result.price)
+        setCategory(result.category)
+        setCompany(result.company)
+    }
 
    const updateProduct = async ()=>{
    console.log(name, price, category, company)
@@ -19,10 +35,10 @@ const UpdateProduct = ()=>{
 
             <input type='text' className='inputBox' value={price} onChange={(e)=>setPrice(e.target.value)} 
             placeholder='Enter Product Price' />
-       
+
             <input type='text' className='inputBox' value={category} onChange={(e)=>setCategory(e.target.value)} 
             placeholder='Enter Product Category' />
-            
+
             <input type='text' className='inputBox' value={company} onChange={(e)=>setCompany(e.target.value)} 
             placeholder='Enter Product Company' />
 
